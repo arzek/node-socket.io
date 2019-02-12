bfffb396baaf8b5b80b89eb5ba9871df9bd55b79
Arzek <arzek2015@gmail.com>
2019-02-13 00:02:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on('connection', client => {
+  client.on('event', data => {
+    client.broadcast.emit(`project-${data.token}`, data)
+  });
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
